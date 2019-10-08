509736e5975c065694299edb28beebce4043f5e0
Oskar Rosen <oskar@oskarrosen.com>
2019-10-09 00:00:25 +0200

    first line of codes of using express

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+
+//Read environment config variables
+require('dotenv').config()
+require('dotenv').config({ path: 'config.env' })
+
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+const PORT = process.env.PORT || 8001
+
+
+app.use(cors())
+
+app.get("/", function(req, res) {
+  console.log("/")
+  res.send()
+})
+
+app.get("/status", function(req, res) {
+  console.log("/status")
+  res.send({ status: "ok" })
+})
+
+app.listen(PORT, function() {
+  console.log(`Calendar API server listening on port ${PORT}`)
+})
+
 console.log("Hello World!")
