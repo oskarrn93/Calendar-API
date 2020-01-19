f09ddec6c0088117d8f4c5c5c3b22de840a73c06
Oskar Rosen <oskar@oskarrosen.com>
2020-01-19 11:23:47 +0100

    removed error handling in util.js and let the routes handle errors

--- util.js
+++ util.js
@@ -1,5 +1,4 @@
 import { default as iCalGenerator } from 'ical-generator'
-import { logger } from './logger.js'
 
 const ONE_HOUR_IN_MS = 3600000
 const TWO_HOURS_IN_MS = 7200000
@@ -7,79 +6,65 @@ const THREE_HOURS_IN_MS = 10800000
 
 export const getCollectionFromDatabase = async (db, collectionName) => {
   const collection = db.handler.collection(collectionName)
-  try {
-    const result = await collection.find({}).toArray()
-    return result
-  } catch (error) {
-    logger.log(error)
-    next(error)
-  }
+  const result = await collection.find({}).toArray()
+  return result
 }
 
 export const getNBA = async db => {
-  try {
-    const listOfGames = await getCollectionFromDatabase(db, 'nba')
-    console.log('listOfGames', listOfGames)
+  const listOfGames = await getCollectionFromDatabase(db, 'nba')
 
-    const iCal = iCalGenerator({
-      domain: 'calendar.oskarrosen.com',
-      name: 'NBA Games',
-      url: 'https://calendar.oskarrosen.com/nba',
-      prodId: '//Oskar Rosen//NBA Games//EN',
-      ttl: 3600,
-      timezone: 'Europe/Berlin',
+  const iCal = iCalGenerator({
+    domain: 'calendar.oskarrosen.com',
+    name: 'NBA Games',
+    url: 'https://calendar.oskarrosen.com/nba',
+    prodId: '//Oskar Rosen//NBA Games//EN',
+    ttl: 3600,
+    timezone: 'Europe/Berlin',
+  })
+
+  listOfGames.forEach(element => {
+    const startTime = new Date(element.timestamp)
+    iCal.createEvent({
+      start: startTime,
+      end: new Date(startTime.getTime() + THREE_HOURS_IN_MS),
+      summary: element.away_team + ' - ' + element.home_team,
+      description: element.away_team + ' - ' + element.home_team,
+      location: element.location,
+      url: 'http://stats.nba.com/schedule/',
+      uid: 'nba-games-' + element._id,
     })
+  })
 
-    listOfGames.forEach(element => {
-      const startTime = new Date(element.timestamp)
-      iCal.createEvent({
-        start: startTime,
-        end: new Date(startTime.getTime() + THREE_HOURS_IN_MS),
-        summary: element.away_team + ' - ' + element.home_team,
-        description: element.away_team + ' - ' + element.home_team,
-        location: element.location,
-        url: 'http://stats.nba.com/schedule/',
-        uid: 'nba-games-' + element._id,
-      })
-    })
-
-    return iCal
-  } catch (error) {
-    throw new Error(error)
-  }
+  return iCal
 }
 
 export const getFootball = async db => {
-  try {
-    const listOfGames = await getCollectionFromDatabase(db, 'football')
-    console.log('listOfGames', listOfGames)
+  const listOfGames = await getCollectionFromDatabase(db, 'football')
+  console.log('listOfGames', listOfGames)
 
-    const iCal = iCalGenerator({
-      domain: 'calendar.oskarrosen.com',
-      name: 'Football Games',
-      url: 'https://calendar.oskarrosen.com/football',
-      prodId: '//Oskar Rosen//Football Games//EN',
-      ttl: 3600,
-      timezone: 'Europe/Berlin',
+  const iCal = iCalGenerator({
+    domain: 'calendar.oskarrosen.com',
+    name: 'Football Games',
+    url: 'https://calendar.oskarrosen.com/football',
+    prodId: '//Oskar Rosen//Football Games//EN',
+    ttl: 3600,
+    timezone: 'Europe/Berlin',
+  })
+
+  listOfGames.forEach(element => {
+    if (isNaN(element.date)) return
+
+    const start = new Date(element.date)
+    const end = new Date(start.getTime() + TWO_HOURS_IN_MS)
+
+    iCal.createEvent({
+      start: start,
+      end: end,
+      summary: element.title,
+      description: element.channels,
+      uid: 'football-games-' + element._id,
     })
+  })
 
-    listOfGames.forEach(element => {
-      if (isNaN(element.date)) return
-
-      const start = new Date(element.date)
-      const end = new Date(start.getTime() + TWO_HOURS_IN_MS)
-
-      iCal.createEvent({
-        start: start,
-        end: end,
-        summary: element.title,
-        description: element.channels,
-        uid: 'football-games-' + element._id,
-      })
-    })
-
-    return iCal
-  } catch (error) {
-    throw new Error(error)
-  }
+  return iCal
 }
